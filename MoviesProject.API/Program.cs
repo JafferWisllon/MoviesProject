9c3b7e4da2703f6d71add1496b58e0fb30d035a2
Jaffer Wisllon <contatojaffer@gmail.com>
2025-03-24 20:25:56 -0300

    feat: moving to a real database

--- MoviesProject.API/Program.cs
+++ MoviesProject.API/Program.cs
@@ -1,13 +1,15 @@
 using MoviesProject.Application;
+using MoviesProject.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
 var app = builder.Build();
 
@@ -22,5 +24,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var DbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await DbInitializer.InitializeAsync();
 
 app.Run();
--- MoviesProject.Contracts/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MoviesProject.Contracts
-{
-    public class Class1
-    {
-
-    }
-}
