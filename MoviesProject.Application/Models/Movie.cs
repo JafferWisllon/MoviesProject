6d24a9fa316815bc2dd2424ee664c01386e61793
Jaffer Wisllon <contatojaffer@gmail.com>
2025-03-24 19:05:28 -0300

    feat: implementing get movie by slug

--- MoviesProject.Application/Models/Movie.cs
+++ MoviesProject.Application/Models/Movie.cs
@@ -1,10 +1,23 @@
-﻿namespace MoviesProject.Application.Models
+﻿using System.Text.RegularExpressions;
+
+namespace MoviesProject.Application.Models
 {
-    public class Movie
+    public partial class Movie
     {
         public required Guid Id { get; init; }
         public required string Title { get; init; }
+        public string Slug => GenerateSlug();
+
         public required int YearOfRelease { get; init; }
         public required List<string> Genres { get; init; } = new();
+        private string GenerateSlug()
+        {
+            var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+                .ToLower().Replace(" ", "-");
+            return $"{sluggedTitle}-{YearOfRelease}";
+        }
+
+        [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 10)]
+        private static partial Regex SlugRegex();
     }
 }
